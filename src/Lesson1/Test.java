4d24b3ea0fdc3d31afac79588c74d24cc65503fe
omgpiu <dyba@list.ru>
2022-07-17 20:57:20 +0300

    init

--- src/Lesson1/Test.java
+++ src/Lesson1/Test.java
@@ -2,6 +2,7 @@ package Lesson1;
 
 public class Test {
     public static void main(String [] args){
+        System.out.println();
         System.out.println("                 РУБАИ     ");
         System.out.println("Много лет размышлял я над жизнью земной.");
         System.out.println("Непонятного нет для меня под луной.");
