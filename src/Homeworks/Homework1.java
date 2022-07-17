57cc70d5a93921a9852ebc61860d72fea8ee3386
omgpiu <dyba@list.ru>
2022-07-17 21:00:09 +0300

    add homework package

---
+++ src/Homeworks/Homework1.java
@@ -0,0 +1,14 @@
+package Homeworks;
+
+public class Homework1 {
+    public static void main(String [] args){
+        System.out.println();
+        System.out.println("                 РУБАИ     ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне извествно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открывая мной.");
+        System.out.println("                                    О. Хайям");
+
+    }
+}
--- src/Lesson1/Test.java
+++ src/Lesson1/Test.java
@@ -2,13 +2,6 @@ package Lesson1;
 
 public class Test {
     public static void main(String [] args){
-        System.out.println();
-        System.out.println("                 РУБАИ     ");
-        System.out.println("Много лет размышлял я над жизнью земной.");
-        System.out.println("Непонятного нет для меня под луной.");
-        System.out.println("Мне извествно, что мне ничего не известно!");
-        System.out.println("Вот последняя правда, открывая мной.");
-        System.out.println("                                    О. Хайям");
 
     }
 }
